3e4642f6e9f53eecb47eb537bfee6f1a95a226ff
Amitya31 <120197886+Amitya31@users.noreply.github.com>
2024-06-18 05:31:26 +0000

    objects2

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -9,7 +9,7 @@ const jsUser = {
     name: "amit",
     "full name": "amit yadav", 
     age: 18,
-    [mySym]: "mykey1",  // declaring symbol in object
+    [mySym]: "mykey1",  // declaring symbol in object by using square brackets
     location: "mumbai",
     email: "amit@gmail.com",
     isLoggedin: "False",
@@ -26,6 +26,19 @@ console.log(jsUser["full name"])
 console.log(jsUser[mySym]) 
 
 jsUser.email = "amit@google.com"
-Object.freeze(jsUser)
+// Object.freeze(jsUser)
 jsUser.email = "amit@microsoft.com"
 console.log(jsUser.email);
+
+jsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this["full name"]}`); // `` is called string interpolation
+}
+// we use $ to print any variable from object 
+// also to refrence the same object we use "this" 
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
