f340acca8bf554a19f5f58a594810688733b1972
Amitya31 <120197886+Amitya31@users.noreply.github.com>
2024-06-25 18:54:14 +0000

    scopes

---
+++ 02_basics/02_scopes.js
@@ -0,0 +1,25 @@
+function one(){
+    const username = "amit"
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+
+
+}
+
+// one();
+
+if (true) {
+    const username = "amit";
+    if (username==="amit") {
+        const website = " youtube";
+        console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
