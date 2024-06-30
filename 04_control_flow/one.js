d42d9cf765b08ce124382e7e9b8d6b6a56f572b3
Amitya31 <120197886+Amitya31@users.noreply.github.com>
2024-06-30 07:19:13 +0000

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+//if
+const isUserloggedIn = true
+const temprature = 41
+
+// if( temprature === 41 ){
+//     console.log("temparture less than 50")
+// } else {
+//     console.log("temprature is above 50")
+// }
+
+
+
+// < , < , <=, >=, ==,!=,
+//=== , !== is used for type checking
+
+const balance = 1000;
+
+if(balance > 500) console.log("test");
