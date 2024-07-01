878d3c45e1a69f5527f34539b600e294f19ea563
Amitya31 <120197886+Amitya31@users.noreply.github.com>
2024-07-01 20:14:50 +0000

    loops

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -8,11 +8,34 @@ for (let i = 0; i<10; i++) {
     //console.log(element)
 }
 
-for (let i = 0; i <= 10; i++) {
-     console.log(`outer loop value: ${i}`)
-    for (let j=0; j<=10; j++){
-     console.log(`Inner loop value ${j} and inner loop ${i}`)
+for (let i = 1; i <= 10; i++) {
+   //  console.log(`outer loop value: ${i}`)
+    for (let j=1; j<=10; j++){
+    // console.log(`Inner loop value ${j} and inner loop ${i}`)
+    //console.log(i + '*' + j + '=' + i*j);
     }
 }
 
+let myArray = ["flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  //console.log(element)
+}
+
+// break and continue
+
+// for (let index = 0; index <=20 ; index++) {
+//   if (index == 5) {
+//     console.log(index);
+//     break;
+//   }
+//   console.log(`value of i = ${index}`);
+// }
+for (let index = 0; index <=20 ; index++) {
+  if (index == 5) {
+    console.log(index);
+    continue;
+  }
+  console.log(`value of i = ${index}`);
+}
 
